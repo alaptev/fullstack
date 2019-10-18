39c86b4ab04664b10bbd0211b6839ecea7c9f9e8
Laptev Anton <antonlaptew@gmail.com>
2019-10-18 17:08:59 +0600

    copy Aticles logic from my-api-client app

---
+++ client/src/components/App.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom';
+
+import '../stylesheets/App.css';
+import Home from './Home';
+import ArticleList from './ArticleList';
+import ArticleInfo from './ArticleInfo';
+import ArticleAdd from './ArticleAdd';
+import ArticleEdit from './ArticleEdit';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div className="container">
+            <Navigation />
+            <Main />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+const Navigation = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/">Home</NavLink></li>
+      <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/articles">Articles</NavLink></li>
+    </ul>
+  </nav>
+);
+
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/articles" component={ArticleList} />
+    <Route exact path="/articles/new" component={ArticleAdd} />
+    <Route exact path="/articles/:id" component={ArticleInfo} />
+    <Route exact path="/articles/:id/edit" component={ArticleEdit} />
+  </Switch>
+);
+
+export default App;
---
+++ client/src/components/ArticleInfo.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class ArticleInfo extends Component {
+  constructor() {
+    super();
+    this.state = { article: {} };
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  componentDidMount() {
+    const domain = 'http://localhost:3004'
+    axios.get(`${domain}/api/articles/${this.props.match.params.id}.json`)
+      .then((response) => {
+        this.setState({
+          article: response.data
+        })
+      })
+      .catch(error => console.log('error', error));
+  }
+
+  handleDelete() {
+    const domain = 'http://localhost:3004'
+    axios.delete(`${domain}/api/articles/${this.props.match.params.id}.json`)
+      .then(() => {
+        // it is essentially a redirect
+        this.props.history.push("/articles")
+      })
+      .catch(error => console.log('error', error));
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.state.article.id}: {this.state.article.title}</h2>
+        <p>{this.state.article.content}</p>
+        <p>
+          <Link to={`/articles/${this.state.article.id}/edit`} className="btn btn-outline-dark">Edit</Link>
+          <button onClick={this.handleDelete} className="btn btn-outline-dark">Delete</button>
+          <Link to="/articles" className="btn btn-outline-dark">Close</Link>
+        </p>
+        <hr/>
+      </div>
+    )
+  }
+}
+
+export default ArticleInfo;
