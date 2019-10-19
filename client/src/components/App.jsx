c1ab610f06b6d8202d9265ce0d1d3ad222083a44
Laptev Anton <antonlaptew@gmail.com>
2019-10-19 12:05:12 +0600

    deleted home page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom';
 
 import '../stylesheets/App.css';
-import Home from './Home';
 import ArticleList from './ArticleList';
 import ArticleInfo from './ArticleInfo';
 import ArticleAdd from './ArticleAdd';
@@ -14,7 +13,6 @@ class App extends Component {
       <div className="App">
         <Router>
           <div className="container">
-            <Navigation />
             <Main />
           </div>
         </Router>
@@ -23,18 +21,8 @@ class App extends Component {
   }
 }
 
-const Navigation = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/">Home</NavLink></li>
-      <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/articles">Articles</NavLink></li>
-    </ul>
-  </nav>
-);
-
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
     <Route exact path="/articles" component={ArticleList} />
     <Route exact path="/articles/new" component={ArticleAdd} />
     <Route exact path="/articles/:id" component={ArticleInfo} />
