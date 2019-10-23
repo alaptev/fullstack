edd4f5fa4d4bf0a5bf167f5965f76152c92454ee
Laptev Anton <antonlaptew@gmail.com>
2019-10-23 14:49:46 +0600

    refactored

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Story.destroy_all
+Article.destroy_all
+
 stories = Story.create([
   {name: "Story One"},
   {name: "Story Two"}
