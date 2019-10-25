cb52dc804ebf25929f924d773c04e2f515f22e7b
Laptev Anton <antonlaptew@gmail.com>
2019-10-25 14:23:23 +0600

    added UI components to filtering and grouping articles

--- client/src/components/ArticleList.jsx
+++ client/src/components/ArticleList.jsx
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import { get } from 'axios';
 import { Link } from 'react-router-dom';
 import { API_HOST, ARTICLE_TYPE } from '../constants';
-import { Table } from 'reactstrap'
+import { Button, Form, FormGroup, Input, InputGroup, InputGroupAddon, Table } from 'reactstrap';
+import Select from 'react-select'
 
 class ArticleList extends Component {
   constructor() {
     super();
-    this.state = { articles: [] };
+    this.state = {
+      filter: '',
+      articles: []
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -18,9 +24,56 @@ class ArticleList extends Component {
       .catch(error => console.log('error', error));
   }
 
+  handleChange(event) {
+    this.setState( { [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const params = {filter: this.state.filter}
+    get(`${API_HOST}/api/stories.json?with_articles=true`, { params: params })
+      .then(response => {
+        this.setState({articles: response.data.storiesWithArticles});
+      })
+      .catch(error => console.log('error', error));
+  }
+
   render() {
+    const state = this.state
+
     return (
       <div>
+        <Form onSubmit={this.handleSubmit} inline>
+          <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">Search</InputGroupAddon>
+              <Input
+                name="filter"
+                type="search"
+                value={state.filter}
+                onChange={this.handleChange}
+                placeholder="search by name and content ..."
+              />
+            </InputGroup>
+          </FormGroup>
+
+          <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">Group by</InputGroupAddon>
+              <Select
+                value={{}}
+                onChange={this.handleStorySelectChange}
+                onInputChange={this.handleStorySelectInputChange}
+                options={[]}
+              />
+            </InputGroup>
+          </FormGroup>
+
+          <Button>Submit</Button>
+        </Form>
+        
+        <br />
+
         <Table hover>
           <thead>
           <tr>
