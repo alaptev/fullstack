eca6bfd3ddff6fe77590a99ecaac86973e3f4b1c
Laptev Anton <antonlaptew@gmail.com>
2019-10-27 12:06:45 +0600

    added test code which uses mobx store

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,13 +4,21 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import '../stylesheets/App.css';
 import ArticleList from './ArticleList';
 import ArticleAddEdit from './ArticleAddEdit';
-import DevTools from 'mobx-react-devtools';
+import { observer } from 'mobx-react';
 
-class App extends Component {
+@observer class App extends Component {
+
+  handleIncrement = () => { this.props.store.increment() };
+  handleDecrement = () => { this.props.store.decrement() };
+
   render() {
     return (
       <div className="App">
-        {/*<DevTools />*/}
+        <h1>{this.props.store.nickName}</h1>
+        <h1>{this.props.store.age}</h1>
+        <button onClick={this.handleDecrement}>-1</button>
+        <button onClick={this.handleIncrement}>+1</button>
+
         <Router>
           <div className="container">
             <Main />
