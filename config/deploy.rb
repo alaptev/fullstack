136e973fc1b4a40348c9c1d9b91a316066e21821
Laptev Anton <antonlaptew@gmail.com>
2019-11-10 20:46:01 +0600

    capistrano ask branch to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "fullstack"
 set :repo_url, "git@github.com:alaptev/fullstack.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
