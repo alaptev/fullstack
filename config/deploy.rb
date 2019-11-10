be8265003351db2f97a95ef4b3a783cbbc22df5d
Laptev Anton <antonlaptew@gmail.com>
2019-11-11 00:09:15 +0600

    dont ask branch during deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,8 @@ set :application, "fullstack"
 set :repo_url, "git@github.com:alaptev/fullstack.git"
 
 # Default branch is :master
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-# set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
